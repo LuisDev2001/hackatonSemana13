363ad532a1d7a551cee9605bbe16dec77bd96e69
Luis Quispe <62967265+lquispeAvatar@users.noreply.github.com>
2020-06-27 11:52:27 -0500

    modal de agregar card, funcionalidad de cerrar y abrir modales

--- reto2/js/main.js
+++ reto2/js/main.js
@@ -1,20 +1,24 @@
 const apperModalClose = () => {
   const modalBody = document.querySelector(".modal-delete");
-  const grayOut = document.querySelector(".gray-out");
+  const grayOut = document.querySelector(".gray-out.close");
+  const formAddPet = document.querySelector("#formAddPet");
+
   let iconsDeleteCard = Array.from(
     document.querySelectorAll(".pet-action-delete")
   );
   iconsDeleteCard.forEach((icon) => {
     icon.addEventListener("click", () => {
+      formAddPet.style.display = "none";
       grayOut.style.display = "flex";
       modalBody.style.display = "flex";
+      console.log("clcik");
     });
   });
 };
 
 const deleteCard = () => {
   const modalBody = document.querySelector(".modal-delete");
-  const grayOut = document.querySelector(".gray-out");
+  const grayOut = document.querySelector(".gray-out.close");
   let iconsDeleteCard = document.querySelector(".pet-action-delete");
 
   let contenedorIcono = iconsDeleteCard.parentNode;
@@ -27,15 +31,46 @@ const deleteCard = () => {
 
 const modal = () => {
   const modalBody = document.querySelector(".modal-delete");
-  const grayOut = document.querySelector(".gray-out");
+  const grayOut = document.querySelector(".gray-out.close");
   const btnCancel = document.querySelector("#no");
   const btnAccept = document.querySelector("#yes");
-  console.log({ modalBody, btnCancel, btnAccept });
   btnAccept.addEventListener("click", deleteCard);
   btnCancel.addEventListener("click", () => {
     modalBody.style.display = "none";
     grayOut.style.display = "none";
   });
 };
+
+const addPet = (e) => {
+  e.preventDefault();
+  console.log("Añadir");
+};
+const modalAddCardPet = () => {
+  const btnAddPet = document.querySelector(".add-pet");
+  const grayOut = document.querySelector(".gray-out");
+  const formAddPet = document.querySelector("#formAddPet");
+  const modalDelete = document.querySelector(".modal-delete");
+  //botones
+  const btnCancel = document.querySelector("#cancel");
+  const btnAccept = document.querySelector("#add");
+  btnAddPet.addEventListener("click", () => {
+    modalDelete.style.display = "none";
+    formAddPet.style.display = "flex";
+    grayOut.style.display = "flex";
+  });
+
+  //Añadir una card
+  btnAccept.addEventListener("click", addPet);
+
+  //Cerrar modal
+  btnCancel.addEventListener("click", (e) => {
+    e.preventDefault();
+    formAddPet.style.display = "none";
+    grayOut.style.display = "none";
+    return false;
+  });
+};
+
 modal();
 apperModalClose();
+modalAddCardPet();
