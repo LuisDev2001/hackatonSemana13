ca87b8ece2c1005a02509544fea00217c3e4fcbd
Luis Quispe <62967265+lquispeAvatar@users.noreply.github.com>
2020-06-27 12:56:28 -0500

    Change structure and position js

--- reto2/js/main.js
+++ reto2/js/main.js
@@ -1,35 +1,3 @@
-const addPet = (e) => {
-  e.preventDefault();
-  //Traer los datos del formulario
-  let name = document.getElementById("name").value;
-  let phone = document.getElementById("phone").value;
-  let email = document.getElementById("email").value;
-  let country = document.getElementById("country").value;
-  let description = document.getElementById("description").value;
-  const contenedorPadre = document.querySelector(".pet > .wrapper");
-  const formAddPet = document.querySelector("#formAddPet");
-  const grayOut = document.querySelector(".gray-out");
-
-  const petInfo = {
-    nombre: name,
-    celular: phone,
-    correo: email,
-    pais: country,
-    descripcion: description,
-  };
-  contenedorPadre.innerHTML += `<article class="pet-card"><div class="pet-action"><span class="pet-action-edit"><i class="far fa-edit"></i>Edit</span><span class="pet-action-delete"><i class="fas fa-times"></i>Delete</span></div><img src="https://placedog.net/500" alt="img-pets" /><div class="pet-information"><div class="pet-name">${petInfo.nombre}</div><div class="pet-phone-mail"><span class="pet-phone">${petInfo.celular}</span><span class="pet-separator">|</span><span class="pet-mail">${petInfo.correo}</span></div><div class="pet-country">${petInfo.pais}</div><div class="pet-description">${petInfo.descripcion}</div></div></article>`;
-
-  modal();
-  apperModalClose();
-  name.value = "";
-  phone.value = "";
-  email.value = "";
-  country.value = "";
-  description.value = "";
-  formAddPet.style.display = "none";
-  grayOut.style.display = "none";
-};
-
 //Modal añadir Pet Formulario
 const modalAddCardPet = () => {
   const btnAddPet = document.querySelector(".add-pet");
@@ -56,7 +24,6 @@ const modalAddCardPet = () => {
     return false;
   });
 };
-
 //Funcion eliminar card
 const deleteCard = () => {
   const modalBody = document.querySelector(".modal-delete");
@@ -70,6 +37,7 @@ const deleteCard = () => {
   modalBody.style.display = "none";
   grayOut.style.display = "none";
 };
+
 //Modal close
 const apperModalClose = () => {
   const modalBody = document.querySelector(".modal-delete");
@@ -100,6 +68,38 @@ const modal = () => {
     modalBody.style.display = "none";
     grayOut.style.display = "none";
   });
+};
+
+const addPet = (e) => {
+  e.preventDefault();
+  //Traer los datos del formulario
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let country = document.getElementById("country").value;
+  let description = document.getElementById("description").value;
+  const contenedorPadre = document.querySelector(".pet > .wrapper");
+  const formAddPet = document.querySelector("#formAddPet");
+  const grayOut = document.querySelector(".gray-out");
+
+  const petInfo = {
+    nombre: name,
+    celular: phone,
+    correo: email,
+    pais: country,
+    descripcion: description,
+  };
+  contenedorPadre.innerHTML += `<article class="pet-card"><div class="pet-action"><span class="pet-action-edit"><i class="far fa-edit"></i>Edit</span><span class="pet-action-delete"><i class="fas fa-times"></i>Delete</span></div><img src="https://placedog.net/500" alt="img-pets" /><div class="pet-information"><div class="pet-name">${petInfo.nombre}</div><div class="pet-phone-mail"><span class="pet-phone">${petInfo.celular}</span><span class="pet-separator">|</span><span class="pet-mail">${petInfo.correo}</span></div><div class="pet-country">${petInfo.pais}</div><div class="pet-description">${petInfo.descripcion}</div></div></article>`;
+
+  modal();
+  apperModalClose();
+  name.value = "";
+  phone.value = "";
+  email.value = "";
+  country.value = "";
+  description.value = "";
+  formAddPet.style.display = "none";
+  grayOut.style.display = "none";
 };
 
 modalAddCardPet();
