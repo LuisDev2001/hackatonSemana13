ea8f54b1fa596225d320514370ba65512e54a3c4
Luis Quispe <62967265+lquispeAvatar@users.noreply.github.com>
2020-06-27 12:40:25 -0500

    Funcionaliadd de agregar cards

--- reto2/js/main.js
+++ reto2/js/main.js
@@ -1,3 +1,33 @@
+const addPet = (e) => {
+  e.preventDefault();
+  //Traer los datos del formulario
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let country = document.getElementById("country").value;
+  let description = document.getElementById("description").value;
+  const contenedorPadre = document.querySelector(".pet > .wrapper");
+  const formAddPet = document.querySelector("#formAddPet");
+  const grayOut = document.querySelector(".gray-out");
+
+  const petInfo = {
+    nombre: name,
+    celular: phone,
+    correo: email,
+    pais: country,
+    descripcion: description,
+  };
+  contenedorPadre.innerHTML += `<article class="pet-card"><div class="pet-action"><span class="pet-action-edit"><i class="far fa-edit"></i>Edit</span><span class="pet-action-delete"><i class="fas fa-times"></i>Delete</span></div><img src="https://placedog.net/500" alt="img-pets" /><div class="pet-information"><div class="pet-name">${petInfo.nombre}</div><div class="pet-phone-mail"><span class="pet-phone">${petInfo.celular}</span><span class="pet-separator">|</span><span class="pet-mail">${petInfo.correo}</span></div><div class="pet-country">${petInfo.pais}</div><div class="pet-description">${petInfo.descripcion}</div></div></article>`;
+
+  name.value = "";
+  phone.value = "";
+  email.value = "";
+  country.value = "";
+  description.value = "";
+  formAddPet.style.display = "none";
+  grayOut.style.display = "none";
+};
+
 const apperModalClose = () => {
   const modalBody = document.querySelector(".modal-delete");
   const grayOut = document.querySelector(".gray-out.close");
@@ -29,6 +59,7 @@ const deleteCard = () => {
   grayOut.style.display = "none";
 };
 
+//Modal de pregunta si es que quiere eliminar la cards
 const modal = () => {
   const modalBody = document.querySelector(".modal-delete");
   const grayOut = document.querySelector(".gray-out.close");
@@ -41,10 +72,6 @@ const modal = () => {
   });
 };
 
-const addPet = (e) => {
-  e.preventDefault();
-  console.log("Añadir");
-};
 const modalAddCardPet = () => {
   const btnAddPet = document.querySelector(".add-pet");
   const grayOut = document.querySelector(".gray-out");
